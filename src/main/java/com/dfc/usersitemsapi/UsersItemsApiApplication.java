84c1182cd911d729729bcb96a094193489255001
dfrancoce <dfrancoce@protonmail.com>
2019-02-04 14:36:20 +0100

    Project initialization

---
+++ src/main/java/com/dfc/usersitemsapi/UsersItemsApiApplication.java
@@ -0,0 +1,14 @@
+package com.dfc.usersitemsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersItemsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersItemsApiApplication.class, args);
+	}
+
+}
+
